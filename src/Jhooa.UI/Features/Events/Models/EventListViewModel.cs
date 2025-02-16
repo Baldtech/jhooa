a176b38c4daf2533efd628172e74c0ca7f0dccc7
Thibault <thibault.ceulemans@outlook.com>
2025-02-16 11:48:25 +0100

    Events + policies UI

---
+++ src/Jhooa.UI/Features/Events/Models/EventListViewModel.cs
@@ -0,0 +1,12 @@
+namespace Jhooa.UI.Features.Events.Models;
+
+public class EventListViewModel
+{
+    public Guid Id { get; init; }
+    public required string Title { get; init; }
+    public string? ImageUrl { get; set; }
+    public DateTimeOffset StartDate { get; init; }
+    public bool IsVisible { get; set; }
+    public bool IsPast => DateTimeOffset.UtcNow > StartDate;
+
+}
