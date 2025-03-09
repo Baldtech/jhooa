789c594f9d439d551efa3934ebe87b0991c7abd6
Thibault <thibault.ceulemans@outlook.com>
2025-03-09 10:22:42 +0100

    wip test

--- src/Jhooa.UI/Webhooks/StripeWebhook.cs
+++ src/Jhooa.UI/Webhooks/StripeWebhook.cs
@@ -1,11 +1,17 @@
+using Jhooa.UI.Data;
+using Jhooa.UI.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using Stripe;
+using Stripe.Checkout;
 
 namespace Jhooa.UI.Webhooks;
 
 [Route("webhooks/stripe")]
 public class StripeWebhook(
+    IStripeService stripeService,
+    ApplicationDbContext dbContext,
     ILogger<StripeWebhook> logger,
     IOptions<Jhooa.UI.Configuration.StripeConfiguration> stripeConfig) : Controller
 {
@@ -37,24 +43,85 @@ public class StripeWebhook(
                         stripeEvent.Id, stripeEvent.Type, subscription?.Id);
                     break;
                 }
-                case EventTypes.ChargeSucceeded:
+                case EventTypes.CheckoutSessionCompleted:
                 {
-                    var charge = stripeEvent.Data.Object as Charge;
+                    var session = stripeEvent.Data.Object as Session;
+                    logger.LogInformation("[Stripe webhook ({StripeEventId} - {EventType})] Checkout session completed: {SessionId}",
+                        stripeEvent.Id, stripeEvent.Type, session?.Id);
+
+                    if (session is null)
+                    {
+                        logger.LogWarning("[Stripe webhook ({StripeEventId} - {EventType})] Unable to parse the session: {SessionId}",
+                            stripeEvent.Id, stripeEvent.Type, session?.Id);
+                        break;
+                    }
                     
+                    var user = await dbContext.Users.FirstOrDefaultAsync(u => u.StripeCustomerId == session.CustomerId);
+
+                    if (user is null)
+                    {
+                        logger.LogWarning("[Stripe webhook ({StripeEventId} - {EventType})] Unable to find user with stripe customer id: {CustomerId}",
+                            stripeEvent.Id, stripeEvent.Type, session.CustomerId);
+                        break;
+                    }
                     
+                    // Subscription
+                    if (session.SubscriptionId != null)
+                    {
+                        var subscriptionId = await stripeService.RetrieveSubscriptionId(session.Id);
+                        logger.LogInformation("Subscription id: {SubscriptionId}", subscriptionId);
+                        
+                        var sub = await dbContext.Subscriptions.FirstOrDefaultAsync(s => s.StripeSessionCheckoutId == session.Id);
+                        if (sub is null)
+                        {
+                            logger.LogWarning("[Stripe webhook ({StripeEventId} - {EventType})] Unable to find subscription with stripe session id: {SessionId}",
+                                stripeEvent.Id, stripeEvent.Type, session.Id);
+                            break;
+                        }
+                        
+                        if (string.Equals(session.PaymentStatus, "Paid", StringComparison.OrdinalIgnoreCase))
+                        {
+                            sub.MarkSubscriptionAsPaid(subscriptionId.Value);
+                            
+                            if (user.ActivatedAt is null)
+                            {
+                                await dbContext.Users.Where(e => e.Id == user.Id)
+                                    .ExecuteUpdateAsync(setters => setters
+                                        .SetProperty(b => b.ActivatedAt, DateTimeOffset.Now)
+                                        .SetProperty(b => b.EmailConfirmed, true));
+                            }
+                        }
+                    }
+                    // Payment intent
+                    else if (session.PaymentIntentId != null)
+                    {
+                        var paymentIntentId = await stripeService.RetrievePaymentIntentIdIfPaid(session.Id);
+                        logger.LogInformation("Payment intent id: {PaymentIntentId}", paymentIntentId);
+                        
+                        var sub = await dbContext.Subscriptions.FirstOrDefaultAsync(s => s.StripeSessionCheckoutId == session.Id);
+                        if (sub is null)
+                        {
+                            logger.LogWarning("[Stripe webhook ({StripeEventId} - {EventType})] Unable to find subscription with stripe session id: {SessionId}",
+                                stripeEvent.Id, stripeEvent.Type, session.Id);
+                            break;
+                        }
+                        
+                        if (string.Equals(session.PaymentStatus, "Paid", StringComparison.OrdinalIgnoreCase))
+                        {
+                            sub.MarkPaymentAsPaid(paymentIntentId.Value);
+                            
+                            if (user.ActivatedAt is null)
+                            {
+                                await dbContext.Users.Where(e => e.Id == user.Id)
+                                    .ExecuteUpdateAsync(setters => setters
+                                        .SetProperty(b => b.ActivatedAt, DateTimeOffset.Now)
+                                        .SetProperty(b => b.EmailConfirmed, true));
+                            }
+                        }
+                    }
+
+                    await dbContext.SaveChangesAsync();
                     
-                    
-                    logger.LogInformation("[Stripe webhook ({StripeEventId} - {EventType})] Charge succeeded: {ChargeId}",
-                        stripeEvent.Id, stripeEvent.Type, charge?.Id);
-                    break;
-                }
-                case EventTypes.PaymentIntentSucceeded:
-                {
-                    var paymentIntent = stripeEvent.Data.Object as PaymentIntent;
-                    
-                    logger.LogInformation(
-                        "[Stripe webhook ({StripeEventId} - {EventType})] Payment intent succeeded: {PaymentIntentId}",
-                        stripeEvent.Id, stripeEvent.Type, paymentIntent?.Id);
                     break;
                 }
                 default:
